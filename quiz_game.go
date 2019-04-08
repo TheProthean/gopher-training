873c5c516149096c17cde4c49544990ee9141e55
Mikhailov Anton <delta.99@mail.ru>
2019-04-08 19:46:25 +0300

    Added timer and reworked the code a little bit.

--- quiz_game.go
+++ quiz_game.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"encoding/csv"
 	"flag"
 	"fmt"
 	"io"
 	"os"
 	"strings"
+	"sync"
+	"time"
 )
 
 type results struct {
@@ -18,6 +21,8 @@ type results struct {
 func main() {
 
 	csvFileName := flag.String("csvfile", "problems.csv", "Name of csv file, where the quiz questions are located.")
+	quizTime := flag.Int("time", 30, "Time restriction on completing quiz.")
+
 	f, err := os.Open(*csvFileName)
 	if err != nil {
 		fmt.Println("Error occured: ", err)
@@ -29,17 +34,37 @@ func main() {
 		total:   0,
 	}
 
-	for {
-		record, err1 := csvReader.Read()
-		if err1 == io.EOF {
-			break
-		} else if err1 != nil {
-			fmt.Println("Error occured: ", err)
-			os.Exit(1)
+	var waiter sync.WaitGroup
+	waiter.Add(1)
+	fmt.Println("Press any button to start quiz!")
+	button := make([]byte, 1)
+	os.Stdin.Read(button)
+
+	go func() {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(*quizTime)*time.Second)
+		defer cancel()
+
+		<-ctx.Done()
+		waiter.Done()
+	}()
+
+	go func() {
+		for {
+			record, err1 := csvReader.Read()
+			if err1 == io.EOF {
+				break
+			} else if err1 != nil {
+				fmt.Println("Error occured: ", err)
+				os.Exit(1)
+			}
+			askQuestion(record, &res)
 		}
-		askQuestion(record, &res)
-	}
-	fmt.Printf("Correct answers: %d. Total questions: %d.\n", res.correct, res.total)
+		waiter.Done()
+	}()
+
+	waiter.Wait()
+
+	fmt.Printf("\nCorrect answers: %d. Total questions: %d.\n", res.correct, res.total)
 }
 
 func askQuestion(quizRecord []string, stats *results) {
@@ -47,7 +72,7 @@ func askQuestion(quizRecord []string, stats *results) {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	answer := scanner.Text()
-	if strings.Compare(strings.ToLower(answer), quizRecord[1]) == 0 {
+	if strings.Compare(strings.ToLower(strings.TrimSpace(answer)), quizRecord[1]) == 0 {
 		stats.correct++
 		fmt.Println("Correct!")
 	} else {
