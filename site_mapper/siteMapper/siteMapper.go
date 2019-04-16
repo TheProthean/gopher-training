7fce72bca11e7e8c53b50a8b3c31086841dcf0e5
Mikhailov Anton <delta.99@mail.ru>
2019-04-16 13:19:21 +0300

    Continued work on Site Map task.

--- site_mapper/siteMapper/siteMapper.go
+++ site_mapper/siteMapper/siteMapper.go
@@ -2,6 +2,7 @@ package siteMapper
 
 import (
 	"encoding/xml"
+	"fmt"
 	"net/http"
 	"os"
 	"strings"
@@ -11,21 +12,23 @@ import (
 
 type node struct {
 	URL       string `xml:"loc"`
-	depth     int    `xml:"depth"`
-	available []node `xml:"url"`
+	Depth     int    `xml:"depth"`
+	Available []node `xml:"url"`
 }
 
 func mapSite(siteName string) (node, error) {
 	allSites := make(map[string]bool)
 	coreNode := node{
 		URL:       siteName,
-		depth:     0,
-		available: []node{},
+		Depth:     0,
+		Available: []node{},
 	}
 	allSites[siteName] = true
 	queue := []node{coreNode}
 	for len(queue) > 0 {
 		currentNode := queue[0]
+		localSites := make(map[string]bool)
+		fmt.Printf("Current node: %+v\n", currentNode)
 		queue = queue[1:]
 		responce, err := http.Get(currentNode.URL)
 		if err != nil {
@@ -33,15 +36,20 @@ func mapSite(siteName string) (node, error) {
 		}
 		defer responce.Body.Close()
 		references, err := htmlParser.ParseHTMLFromSource(responce.Body)
+		fmt.Printf("\tFound hrefs: %v\n", references)
 		if err != nil {
 			return node{}, err
 		}
 		for _, v := range references {
+			fmt.Println("Starting new iteration")
 			var newNodeURL string
-			if v.Reference[0] == '/' {
-				newNodeURL = currentNode.URL + v.Reference
+			if v.Reference[0] == '#' || len(v.Reference) == 0 || (len(v.Reference) == 1 && v.Reference[0] == '/') {
+				continue
+			}
+			if v.Reference[0] == '/' && v.Reference[1] != '/' {
+				newNodeURL = siteName + v.Reference
 			} else {
-				mainDomain := strings.Split(currentNode.URL, "/")[2]
+				mainDomain := strings.Split(siteName, "/")[2]
 				refDomain := strings.Split(v.Reference, "/")[2]
 				if mainDomain == refDomain {
 					newNodeURL = v.Reference
@@ -51,15 +59,18 @@ func mapSite(siteName string) (node, error) {
 			}
 			newNode := node{
 				URL:       newNodeURL,
-				depth:     currentNode.depth + 1,
-				available: []node{},
+				Depth:     currentNode.Depth + 1,
+				Available: []node{},
 			}
-			currentNode.available = append(currentNode.available, newNode)
-			if _, found := allSites[newNodeURL]; found {
-				continue
+			fmt.Printf("\tFound node: %+v\n", newNode)
+			if _, found := localSites[newNodeURL]; !found {
+				currentNode.Available = append(currentNode.Available, newNode)
+				localSites[newNodeURL] = true
 			}
-			queue = append(queue, newNode)
-			allSites[newNodeURL] = true
+			if _, found := allSites[newNodeURL]; !found {
+				queue = append(queue, newNode)
+				allSites[newNodeURL] = true
+			}
 		}
 	}
 	return coreNode, nil
--- site_mapper/siteMapperUI.go
+++ site_mapper/siteMapperUI.go
@@ -3,13 +3,21 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strings"
 
 	siteMapper "github.com/gopher-training/site_mapper/siteMapper"
 )
 
 func main() {
-	urlflag := flag.String("url", "golang.org", "URL of site, that will be mapped")
-	err := siteMapper.PrintSiteMap(*urlflag)
+	urlflag := flag.String("url", "http://golang.org", "URL of site, that will be mapped")
+	var newURL string
+	if (*urlflag)[len(*urlflag)-1] == '/' {
+		newURL = *urlflag
+	} else {
+		newURL = *urlflag + string('/')
+	}
+	newURL = strings.Replace(newURL, "www.", "http://", 1)
+	err := siteMapper.PrintSiteMap(newURL)
 	if err != nil {
 		fmt.Println("Error occured: ", err)
 	}
