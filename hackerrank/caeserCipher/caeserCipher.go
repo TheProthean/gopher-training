6e6a7b7c651807ffac6a234ab96efa7bdd24319b
TheProthean <44200616+TheProthean@users.noreply.github.com>
2019-04-17 15:05:04 +0300

    Update caeserCipher.go

--- hackerrank/caeserCipher/caeserCipher.go
+++ hackerrank/caeserCipher/caeserCipher.go
@@ -13,25 +13,25 @@ import (
 func caesarCipher(s string, k int32) string {
 	alphabetSize := 26
 	modK := int(k) % alphabetSize
-	newString := make([]byte, len(s))
+	result := make([]byte, len(s))
 	for i := range s {
 		if s[i] >= 97 && s[i] <= 122 {
 			newB := s[i] + byte(modK)
 			if newB > 122 {
 				newB -= byte(alphabetSize)
 			}
-			newString[i] = newB
+			result[i] = newB
 		} else if s[i] >= 65 && s[i] <= 90 {
 			newB := s[i] + byte(modK)
 			if newB > 90 {
 				newB -= byte(alphabetSize)
 			}
-			newString[i] = newB
+			result[i] = newB
 		} else {
-			newString[i] = s[i]
+			result[i] = s[i]
 		}
 	}
-	return string(newString)
+	return string(result)
 }
 
 func main() {
