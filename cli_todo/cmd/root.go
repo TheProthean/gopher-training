cf24a82b3a77bde2ab2acab862a3d97891668c28
Mikhailov Anton <delta.99@mail.ru>
2019-04-17 17:07:26 +0300

    Started working on CLI part in CLI TODO task.

---
+++ cli_todo/cmd/root.go
@@ -0,0 +1,9 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+//RootCmd is a main command for cobra CLI package
+var RootCmd = &cobra.Command{
+	Use:   "TODO",
+	Short: "TODO is a CLI task manager.",
+}
